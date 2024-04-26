73c75e18d1f3d07627ed8d88132250acd6e01c0a
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-04-26 12:00:30 +0530

    FEAT: Some changes

--- FastApi/app/main.py
+++ FastApi/app/main.py
@@ -59,14 +59,14 @@ def get_posts(db: Session = Depends(get_db)):
     # posts = cursor.fetchall()
 
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 # -->
 
 # Create post -->
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
     #                (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
@@ -79,7 +79,7 @@ def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": new_post}
+    return new_post
 # -->
 
 
@@ -117,7 +117,7 @@ def delete_post(id: int,  db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: schemas.Post,  db: Session = Depends(get_db)):
+def update_post(id: int, post: schemas.PostCreate,  db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
     #                (post.title, post.content, post.published, str(id)))
     # updated_post = cursor.fetchone()
