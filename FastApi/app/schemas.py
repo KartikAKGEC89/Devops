d1ea547ceee8d79020d22acc126db7dace2a4010
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-04-26 12:47:39 +0530

    FEAT: Response model

--- FastApi/app/schemas.py
+++ FastApi/app/schemas.py
@@ -8,3 +8,11 @@ class PostBase(BaseModel):
 
 class PostCreate(PostBase):
     pass
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool
+    
+    class config:
+        orm_mode = True
