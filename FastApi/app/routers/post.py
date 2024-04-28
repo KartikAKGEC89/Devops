28c4f49f73aef3f8e44f1effb2db40e1cba35e72
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-04-28 15:11:21 +0530

    FEAT: User prefix method for router to minimize the routes

--- FastApi/app/routers/post.py
+++ FastApi/app/routers/post.py
@@ -3,11 +3,13 @@ from .. import models, schemas
 from ..database import get_db
 from sqlalchemy.orm import Session
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/posts"
+)
 
 
 # Get data -->
-@router.get("/posts")
+@router.get("/")
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts """)
     # posts = cursor.fetchall()
@@ -19,7 +21,7 @@ def get_posts(db: Session = Depends(get_db)):
 
 # Create post -->
 
-@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
     #                (post.title, post.content, post.published))
@@ -39,7 +41,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
 # Get post by id -->
 
-@router.get("/posts/{id}")
+@router.get("/{id}")
 def get_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * from posts WHERE id = %s """, (str(id),))
     # post = cursor.fetchone()
@@ -54,7 +56,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 
 # Delete post -->
-@router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int,  db: Session = Depends(get_db)):
     # cursor.execute(
     #     """DELETE FROM posts WHERE id = %s returning *""", (str(id),))
@@ -70,7 +72,7 @@ def delete_post(id: int,  db: Session = Depends(get_db)):
 # -->
 
 
-@router.put("/posts/{id}")
+@router.put("/{id}")
 def update_post(id: int, post: schemas.PostCreate,  db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
     #                (post.title, post.content, post.published, str(id)))
