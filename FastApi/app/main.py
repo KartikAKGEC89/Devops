275c3443ad61edbfd15583c02695daa9e3af7c74
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-05-04 23:24:22 +0530

    FIX: Main.py file

--- FastApi/app/main.py
+++ FastApi/app/main.py
@@ -1,13 +1,13 @@
-from typing import Optional
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy.orm import Session
-from . import models, schemas, utils
-from .database import engine, get_db
+from fastapi import FastAPI
+
+# Partof connection import -->
+# import psycopg2
+# from psycopg2.extras import RealDictCursor
+# import time
+# -->
+
+from . import models
+from .database import engine
 from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
@@ -16,31 +16,22 @@ app = FastAPI()
 
 
 
-# Connection -->
+# # Connection -->
 
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', database='FastApi', user='postgres',
-                                password='Deepak90@', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was succesfull!")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error: ", error)
-        time.sleep(2)
-# -->
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='FastApi', user='postgres',
+#                                 password='Deepak90@', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was succesfull!")
+#         break
+#     except Exception as error:
+#         print("Connecting to database failed")
+#         print("Error: ", error)
+#         time.sleep(2)
+# # -->
 
-my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {
-    "title": "favorite foods", "content": "I like pizza", "id": 2}]
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
@@ -52,10 +43,3 @@ app.include_router(user.router)
 app.include_router(auth.router)
 
 # -->
-
-# SQLAlchemy get data from database -->
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"status": posts}
-# --->
